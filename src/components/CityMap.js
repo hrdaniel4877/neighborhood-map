d6db73e1529545fb0a69331eebe3f6c5283b46cb
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2018-07-05 10:54:58 -0400

    Display a Google Map centered on Cluj-Napoca city, Romania

--- src/components/CityMap.js
+++ src/components/CityMap.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
+import {Map, GoogleApiWrapper} from 'google-maps-react'
 
 class CityMap extends Component {
 	render() {
 		return (
-			<div> Here will go a map </div>
+			<Map
+				google={this.props.google} 
+				initialCenter={{lat:46.769994, lng:23.589588}} 
+				zoom={14} 
+			>
+			</Map>
 		)
 	}
 }
 
-export default CityMap
+export default GoogleApiWrapper({
+	apiKey: 'AIzaSyDh30lryFckQPoY4h0Mp8d1L1bsMh3rQLQ'
+})(CityMap)
