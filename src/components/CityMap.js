2c48e5ea0b756046f27df1c3521571aa21b84ae3
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2018-07-05 18:14:40 -0400

    Add markers to the map

--- src/components/CityMap.js
+++ src/components/CityMap.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react'
-import {Map, GoogleApiWrapper} from 'google-maps-react'
+import {Map, GoogleApiWrapper, Marker} from 'google-maps-react'
 
 class CityMap extends Component {
 	render() {
+
 		return (
 			<Map
 				google={this.props.google} 
 				initialCenter={{lat:46.769994, lng:23.589588}} 
-				zoom={14} 
+				zoom={17} 
 			>
+
+			{
+				this.props.locations.map(location => {
+					return (
+						<Marker 
+							key={location.id} 
+							position={{ lat: location.position.lat, lng: location.position.lng}} 
+							title={location.title} 
+							animation={this.props.google.maps.Animation.Fo}
+			                category={location.category}
+			                address={location.address}
+			                crossStreet={location.crossStreet}
+			                state={location.state}
+			                coordinates={location.coordinates}
+			                postalCode={location.postalCode}
+						/>
+					)
+				})
+			}
+
 			</Map>
 		)
 	}
